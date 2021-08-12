e1c712250612efdef79b48425ce39788200769f5
AndreyKrauthein <54869693+AndreyKrauthein@users.noreply.github.com>
2021-08-12 20:03:33 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:app_magazineluiza/views/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Magazine Luiza",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
